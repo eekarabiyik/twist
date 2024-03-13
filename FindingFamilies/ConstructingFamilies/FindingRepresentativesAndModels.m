5d9a2653f5ccb5a50d2345a18d61e8e5bfd6c972
eray <ek693@cornell.edu>
2024-03-13 16:17:21 -0400

    mistakeinthis file

--- FindingFamilies/ConstructingFamilies/FindingRepresentativesAndModels.m
+++ FindingFamilies/ConstructingFamilies/FindingRepresentativesAndModels.m
@@ -30,7 +30,6 @@ end for;
 "Finding Representatives";
 for k in Keys(FAM) do
     print(k);
-    if FAM[k]`genus lt 3 then continue; end if;
     time0:=Realtime();
     
         T:=FindSpecialSubgroup(FAM[k]`calG,FAM[k]`B);
