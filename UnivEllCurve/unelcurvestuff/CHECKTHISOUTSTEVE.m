d0459ec98197ea6dedb546fd20b25ba13c88da50
eray <ek693@cornell.edu>
2025-03-13 00:57:36 -0400

    fix

--- UnivEllCurve/unelcurvestuff/CHECKTHISOUTSTEVE.m
+++ UnivEllCurve/unelcurvestuff/CHECKTHISOUTSTEVE.m
@@ -1,17 +1,29 @@
 //Load  this to run the code on all curves in LMFDB
 //Be careful about the paths, havent fixed them yet
 
-ChangeDirectory("/homes/ek693/Main/FindingFamilies");
+ChangeDirectory("./FindingFamilies");
 AttachSpec("../spec");
-FAM:=LoadFamilies(["/homes/ek693/Main/UnivEllCurve/unelcurvestuff/allfamincludingfinestuffLATEST.dat"]);
+// the file below doesn't exist 
+//FAM:=LoadFamilies(["/homes/ek693/Main/UnivEllCurve/unelcurvestuff/allfamincludingfinestuffLATEST.dat"]);
+FAM := LoadFamilies(["./FamilyDataFiles/Families1new.dat"]);
 //It is enough to load Main/FindingFamilies/FamilyDataFiles/Families1new.dat and Main/FindingFamilies/FamilyDataFiles/Families2new.dat
 //for you
 //Will update these once things are optimized and clean.
-load "/homes/ek693/Main/UnivEllCurve/univellcurvedatalmfdb/lmfdbdatafine.m";
+load "../UnivEllCurve/univellcurvedatalmfdb/lmfdbdatafine.m";
 curves:=make_data();
 
+k := 101;
+G := curves[k]`subgroup;
+M0 := CreateModularCurveRec(G);
+H := GL2IncludeNegativeOne(G);
+M := CreateModularCurveRec(H);
+TY := SL2Intersection(H);
+famkey := FamilyFinder(H, TY, FAM);
+M := FindModelOfXG(M : G0:=FAM[famkey]`calG);
+_, jmap, _, _, _, _, _ := AbsoluteJmap(M);
 
 
+/*
 for k in Keys(curves) do
 if k in [1..100] then continue; end if;
 G:=curves[k]`subgroup;
@@ -40,3 +52,4 @@ if #BaseRing(G) eq Infinity() then continue; end if;
         print(Realtime(time0));
         
 end for;
+*/
